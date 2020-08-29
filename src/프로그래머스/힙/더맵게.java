2348dd53d155d0fd46d8dafa274ff5d366f5e988
wenodev <logicalstark1105@gmail.com>
2020-08-29 14:48:54 +0900

    더 맵게 수정

---
+++ src/프로그래머스/힙/더맵게.java
@@ -0,0 +1,26 @@
+package 프로그래머스.힙;
+
+public class 더맵게 {
+    public static void main(String[] args) {
+
+        int[] scoville = new int[]{1, 2, 3, 4, 10, 11, 12, 13, 14, 16};
+        int K = 1000;
+
+        Solution(scoville, K);
+    }
+
+    public static int Solution(int[] scoville, int K) {
+        int answer = 0;
+
+
+
+
+
+
+
+
+
+
+        return answer;
+    }
+}
