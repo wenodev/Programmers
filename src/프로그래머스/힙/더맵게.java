82e6c9ee7d33eb5c5f875230bb4db2954449b4a4
wenodev <logicalstark1105@gmail.com>
2020-09-03 21:59:24 +0900

    더맵게 수정중 0903

--- src/프로그래머스/힙/더맵게.java
+++ src/프로그래머스/힙/더맵게.java
@@ -1,8 +1,6 @@
 package 프로그래머스.힙;
 
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class 더맵게 {
@@ -20,48 +18,30 @@ public class 더맵게 {
         int temp = 0;
 
         List<Integer> scovilleList = new ArrayList<>();
-        for(int num : scoville){
+        for (int num : scoville) {
             scovilleList.add(num);
         }
 
-        for(int num : scovilleList){
-            System.out.print(num + " ");
-        }
-        System.out.println();
 
-        for(int i=0; i<scovilleList.size(); i++){
+        do {
 
-            if (scovilleList.get(0) >= K){
+            if (scovilleList.get(0) >= K) {
                 break;
             }
 
-            temp = scovilleList.remove(0) + (scovilleList.remove(0)*2);
-            for(int j=0; j<scovilleList.size(); j++){
-                if(temp < scovilleList.get(j)){
-                    scovilleList.add(j,temp);
+            temp = scovilleList.remove(0) + (scovilleList.remove(0) * 2);
+            for (int j = 0; j < scovilleList.size(); j++) {
+                if (temp < scovilleList.get(j)) {
+                    scovilleList.add(j, temp);
+                    break;
+                }
+                if (temp > scovilleList.get(scovilleList.size() - 1)) {
+                    scovilleList.add(temp);
                     break;
                 }
             }
             answer++;
-        }
-
-        System.out.println("K : " + K);
-        System.out.println("섞은 회수 : " + answer);
-        System.out.print("scovilleList : ");
-        for (int i : scovilleList){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-
-        if (scovilleList.size() == 1){
-            if(K > scovilleList.get(0)){
-                answer = -1;
-                return answer;
-            }
-        }
-
-
+        }while (scovilleList.size() != 2);
 
         return answer;
     }
