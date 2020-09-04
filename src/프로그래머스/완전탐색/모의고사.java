d0f0a0ac57f5f6186dcbb86566ef5f80544427c3
wenodev <logicalstark1105@gmail.com>
2020-09-05 07:01:28 +0900

    모의고사 진행중 0905

--- src/프로그래머스/완전탐색/모의고사.java
+++ src/프로그래머스/완전탐색/모의고사.java
@@ -1,22 +1,25 @@
 package 프로그래머스.완전탐색;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class 모의고사 {
 
     public static void main(String[] args) {
-        int[] answers = {1,5,1};
+        int[] answers = {3,3,2};
         Solution(answers);
 
     }
 
-    public static int[] Solution(int[] answers){
+    public static void Solution(int[] answers){
         int[] count = new int[]{0,0,0};
-
         int[] person1 = new int[]{1, 2, 3, 4, 5};
         int[] person2 = new int[]{2, 1, 2, 3, 2, 4, 2, 5};
         int[] person3 = new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
+        List<Integer> answerList = new ArrayList<>();
 
 
         for(int i=0; i<answers.length; i++){
@@ -25,13 +28,23 @@ public class 모의고사 {
             }
         }
 
-        System.out.println(count[0]);
+        for(int i=0; i<answers.length; i++){
+            if (answers[i] == person2[i%person2.length]){
+                count[1]++;
+            }
+        }
 
+        for(int i=0; i<answers.length; i++){
+            if (answers[i] == person3[i%person3.length]){
+                count[2]++;
+            }
+        }
 
-
-
-        return count;
-
+        if (count[0] < count[1]){
+            answerList.add(2);
+        }else if(count[0] > count[1]){
+            answerList.add(1);
+        }
 
     }
 }
