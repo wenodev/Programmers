79a1827a080ede399bbb65248cb2e69a1728ce6f
wenodev <logicalstark1105@gmail.com>
2020-11-08 12:50:24 +0900

    [update] 전화번호목록 주석

--- src/프로그래머스/해시/전화번호목록.java
+++ src/프로그래머스/해시/전화번호목록.java
@@ -13,6 +13,11 @@ public class 전화번호목록 {
 
     public static boolean Solution(String[] phone_book) {
 
+        /*
+        - 코드의 효율성을 위해 바로 return 하는 함수를 항상 고려해야함
+        - 접두사 문제는 startsWith 고려하기
+        */
+
         for (int i = 0; i < phone_book.length-1; i++) {
             for (int j= i+1; j < phone_book.length; j++){
                 if (phone_book[i].startsWith(phone_book[j])) return false;
