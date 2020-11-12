e1fe863a9c1f1f61662bfd1de91f192f7d1ded23
wenodev <logicalstark1105@gmail.com>
2020-11-12 23:01:14 +0900

    [create] 위장

---
+++ src/프로그래머스/해시/위장.java
@@ -0,0 +1,41 @@
+package 프로그래머스.해시;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class 위장 {
+
+
+    public static int solution(String[][] clothes ){
+        int answer = 1;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < clothes.length; i++) {
+
+            if (!map.containsKey(clothes[i][1])){
+                map.put(clothes[i][1], 1);
+            }else{
+                map.put(clothes[i][1], map.get(clothes[i][1]) + 1);
+            }
+        }
+
+        for (String key : map.keySet()){
+            answer *= map.get(key) + 1;
+        }
+
+
+        return answer-1;
+    }
+
+    public static void main(String[] args) {
+
+        String[][] clothes = {
+                {"crow_mask", "face"},
+                {"blue_sunglasses", "face"},
+                {"smoky_makeup", "face"}
+        };
+        System.out.println(solution(clothes));
+    }
+
+}
