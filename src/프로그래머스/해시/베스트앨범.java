2973b70c84a86465eb19d6bef379680a2bfd526a
wenodev <logicalstark1105@gmail.com>
2020-11-15 23:47:29 +0900

    [create] 베스트앨범 시작

---
+++ src/프로그래머스/해시/베스트앨범.java
@@ -0,0 +1,30 @@
+package 프로그래머스.해시;
+
+public class 베스트앨범 {
+
+    public static int[] solution(String[] genres, int[] plays){
+        int[] answer = {};
+
+
+
+
+
+
+
+
+
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 250};
+
+        solution(genres, plays);
+
+    }
+
+}
