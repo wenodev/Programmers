cef04244a69048849cd03a725fb10f5525b387ab
wenodev <logicalstark1105@gmail.com>
2020-11-21 23:46:21 +0900

    [create] 기능개발

---
+++ src/프로그래머스/스택큐/기능개발.java
@@ -0,0 +1,28 @@
+package 프로그래머스.스택큐;
+
+public class 기능개발 {
+
+
+    public static int[] solution(int[] progresses, int[] speeds){
+        int[] answer = {};
+
+
+
+
+
+
+
+
+
+
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] progresses = new int[]{93, 30, 55};
+        int[] speeds = new int[]{1,30,5};
+        solution(progresses, speeds);
+    }
+
+}
--- src/프로그래머스/해시/베스트앨범.java
+++ src/프로그래머스/해시/베스트앨범.java
@@ -1,12 +1,56 @@
 package 프로그래머스.해시;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class 베스트앨범 {
+
+    static class AlbumInfo{
+
+        String genres;
+        int plays;
+        int index;
+
+        public AlbumInfo(String genres, int plays, int index){
+            this.genres = genres;
+            this.plays = plays;
+            this.index = index;
+        }
+
+        public String getGenres() {
+            return genres;
+        }
+
+        public void setGenres(String genres) {
+            this.genres = genres;
+        }
+
+        public int getPlays() {
+            return plays;
+        }
+
+        public void setPlays(int plays) {
+            this.plays = plays;
+        }
+
+        public int getIndex() {
+            return index;
+        }
+
+        public void setIndex(int index) {
+            this.index = index;
+        }
+    }
 
     public static int[] solution(String[] genres, int[] plays){
         int[] answer = {};
 
 
+        for (int i = 0; i < genres.length; i++) {
+            AlbumInfo albumInfo = new AlbumInfo(genres[i], plays[i], i);
+        }
 
+        
 
 
 
