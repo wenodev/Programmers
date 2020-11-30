db5aed813587238dd0d63201519d6876698eb366
wenodev <logicalstark1105@gmail.com>
2020-11-30 23:58:23 +0900

    [update] 모의고사 완료

--- src/프로그래머스/모의고사.java
+++ src/프로그래머스/모의고사.java
@@ -1,34 +1,46 @@
 package 프로그래머스;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class 모의고사 {
 
-
     public static int[] solution(int[] answers){
-        int[] answer = {};
+
 
         int[] person1 = {1, 2, 3, 4, 5};
         int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
 
-        List<Integer> countList = new ArrayList<>(3);
+        List<Integer> answersList = new ArrayList<>(Arrays.asList(0,0,0));
+        List<Integer> countList = new ArrayList<>();
+
 
         for (int i = 0; i < answers.length; i++) {
             if (answers[i] == person1[i%person1.length]){
-                countList.set(0, countList.get(0) + 1);
-            }else if(answers[i] == person1[i%person2.length]){
-                countList.set(1, countList.get(1) + 1);
-            }else if(answers[i] == person1[i%person3.length]){
-                countList.set(2, countList.get(2
-
-                ) + 1);
+                answersList.set(0,answersList.get(0) + 1);
+            }
+            if (answers[i] == person2[i%person2.length]){
+                answersList.set(1,answersList.get(1) + 1);
+            }
+            if (answers[i] == person3[i%person3.length]){
+                answersList.set(2,answersList.get(2) + 1);
             }
         }
 
-        for (int i = 0; i < count.length; i++) {
-            System.out.println(count[i] + " ");
+        int max = Collections.max(answersList);
+
+        for (int i = 0; i < answersList.size(); i++) {
+            if (answersList.get(i) == max){
+                countList.add(i+1);
+            }
+        }
+
+        int[] answer = new int[countList.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = countList.get(i);
         }
 
 
@@ -36,7 +48,7 @@ public class 모의고사 {
     }
 
     public static void main(String[] args) {
-        int[] answers = {1,2,3,4,5};
+        int[] answers = {1,3,2,4,2};
         solution(answers);
     }
 }
