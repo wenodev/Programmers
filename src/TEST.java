a7ff8ba3eb80662df7a326f28b1b172a58ce3cd7
wenodev <logicalstark1105@gmail.com>
2020-12-19 23:34:41 +0900

    [update] git test 1219

--- src/TEST.java
+++ src/TEST.java
@@ -5,6 +5,8 @@ public class TEST {
 
     public static void main(String[] args) {
 
+        //test
+
         String[] arr = {"abc" , "abc"};
 
         System.out.println(arr[0].indexOf(arr[1]));
